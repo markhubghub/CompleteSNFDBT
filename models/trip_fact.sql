1aa0365254069235d08094a0e4affce2e344af7c
mark hubbard <markhub+dbt@gmail.com>
2025-10-22 21:54:52 +0000

    save up changes to mins on fact table

--- models/trip_fact.sql
+++ models/trip_fact.sql
@@ -8,8 +8,8 @@ with
             date(to_timestamp(started_at)) as trip_start_date,
             date(to_timestamp(ended_at)) as trip_end_date,
             timestampdiff(
-                second, to_timestamp(started_at), to_timestamp(ended_at)
-            ) as trip_duration_seconds,
+                minute, to_timestamp(started_at), to_timestamp(ended_at)
+            ) as trip_duration_minutes,
             start_statio_id as trip_station_start_id,
             end_station_id as trip_station_end_id,
             member_casual,
