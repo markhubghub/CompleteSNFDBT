0b35b22bdf75592cb4a87ae319842c83cedeeaa7
mark hubbard <markhub+dbt@gmail.com>
2025-10-22 02:35:11 +0000

    station dimension

---
+++ models/Station_Dimension.sql
@@ -0,0 +1,17 @@
+with
+    bike as (
+
+        select
+            start_statio_id as station_id,
+            start_station_name as station_name,
+            start_lat as station_lat,
+            start_lng as station_long
+
+        from {{ source("demo", "BIKE") }}
+        where ride_id <> 'ride_id'
+        -- limit 10
+
+    )
+
+select *
+from bike
